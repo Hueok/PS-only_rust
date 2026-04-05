84b263ca1e3771a38caa01f0d80236ed6d882592
Hueok <developerhueok@gmail.com>
2026-04-05 19:06:32 +0900

    modifying..

--- p15829/src/main.rs
+++ p15829/src/main.rs
@@ -1,7 +1,7 @@
 use std::io::{self, Read};
 
-const P: usize = 1234567891;
-const R: usize = 31;
+const P: i64 = 1234567891;
+const R: i64 = 31;
 
 fn main() {
     let mut input = String::new();
@@ -12,15 +12,30 @@ fn main() {
 
     let unistring: Vec<char> = it.next().unwrap().chars().collect();
 
-    let mut sum = 0;
+    let mut sum: i64 = 0;
 
     for (i, &alp) in unistring.iter().enumerate() {
-        sum += ctoi(alp) * R.pow(i as u32) % P;
+        // sum += ctoi(alp) * R.pow(i as u32) % P;
+        sum += (ctoi(alp) * modular_loop(i as u32)) % P;
     }
 
     println!("{}", sum);
 }
 
-fn ctoi(c: char) -> usize {
-    (c as u8 - b'a') as usize + 1
+fn ctoi(c: char) -> i64 {
+    (c as u8 - b'a') as i64 + 1
 }
+fn modular_loop(exp: u32) -> i64{
+    if exp == 0 {
+        return 1;
+    }
+    if exp == 1 {
+        return R;
+    }
+    let mut acc = R;
+    for i in 2..=exp {
+        acc = (acc * R) % P;
+    }
+    acc
+}
+
