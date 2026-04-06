e0d3412970aeee318ab0bc363894bef26c030fab
Hueok <developerhueok@gmail.com>
2026-04-06 14:47:38 +0900

    refactoring p1259

--- p1259/src/main.rs
+++ p1259/src/main.rs
@@ -9,26 +9,10 @@ fn main() {
         if s == String::from('0') {
             break;
         }
-        let size: usize = s.len();
-        let numstring: i32 = s.parse().unwrap();
-        let mut is_rom: bool = true;
+        let is_pal = (0..s.len()/2) 
+            .all(|i| s.as_bytes()[i] == s.as_bytes()[s.len()-1-i]);
 
-        for i in 1..=size/2{
-            let divisor = 10_i32.pow((size-i) as u32);
-            let lhs: i32 = (numstring / divisor) % 10;
-            let rhs: i32 = (numstring % 10_i32.pow(i as u32)) / 10_i32.pow((i-1) as u32);
-            if lhs != rhs {
-                is_rom = false;
-            }
-        }
-        
-        let mut result = String::new();
-        if is_rom {
-            result.push_str("yes");
-        } else {
-            result.push_str("no");
-        }
-        println!("{}", result);
+        println!("{}", if is_pal {"yes"} else {"no"});
 
 
     }
